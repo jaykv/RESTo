15e5e1aa7e6b215232a7d173640c254c915f3773
jaykv <improcuziam@gmail.com>
2022-08-06 10:41:17 -0400

    Restructuring part 2

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import find_packages, setup
+from resto import __VERSION__
 
 setup(
-    name='RESTo',
-    version='0.1.0',
+    name='resto',
+    version=__VERSION__,
     description='REST API framework for the lazy',
     packages=find_packages(include=['resto', 'resto.*']),
-    install_requires=[],
+    install_requires=[
+        "pydantic >= 1.9.1",
+        "spectree >= 0.10.0"
+    ],
 )
