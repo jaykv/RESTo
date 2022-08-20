ab37580c82d3f50f3168d02bbd32421b424cfeae
jaykv <improcuziam@gmail.com>
2022-08-20 17:36:14 -0400

    Update setup script

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ setup(
     name='resto',
     version=__VERSION__,
     description='REST API framework for the lazy',
+    author='Jay',
+    license='MIT',
+    classifiers=['Development Status :: 3 - Alpha', 'License :: OSI Approved :: MIT License'],
     packages=find_packages(include=['resto', 'resto.*']),
-    install_requires=["pydantic >= 1.9.1", "spectree >= 0.10.0"],
+    python_requires='>=3.9',
+    install_requires=['pydantic >= 1.9.1', 'spectree >= 0.10.0'],
+    extras_require={'testing': ['flask', 'mongoframes', 'pymongo']},
 )
